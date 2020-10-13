bc5c045ace73c8781681e81bd33d107dc6c81009
beebeebeeebeee <bee.1845698825@gmail.com>
2020-10-13 16:52:06 +0800

    init node.js

---
+++ web shop/main.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.listen(port, function () {
+    console.log('Express app started on ' + port);
+});
