c5ac95ccd762f0cd53de29b9172f10e04d4fbcca
chan9948 <chan9948@gmail.com>
2020-10-17 21:53:36 +0800

    css update

--- web shop/public/js/app.js
+++ web shop/public/js/app.js
@@ -1,35 +1,35 @@
 window.addEventListener("DOMContentLoaded", (evt) => {
-  fetch_items();
+    fetch_items();
 });
 
 var items = [];
 const fetch_items = () => {
-  fetch("/api/items/all")
-    .then((res) => {
-      return res.json();
-    })
-    .then((res) => {
-      const section = document.getElementById("products-section");
-      items = res.body;
+    fetch("/api/items/all")
+        .then((res) => {
+            return res.json();
+        })
+        .then((res) => {
+            const section = document.getElementById("products-section");
+            items = res.body;
 
-      section.innerHTML = "";
-      items.forEach((element) => {
-        section.innerHTML += `
+            section.innerHTML = "";
+            items.forEach((element) => {
+                section.innerHTML += `
       <div class="product-cell">
           <p class="product-name">${element.name}</p>
           <img class="product-img" src="./img/${element.img}"></img>
           <p class="product-price">$${element.price}</p>
           <p class="product-desc">${element.desc}</p>
-          <button onclick="buy(${element.id})" class="btn btn-success"><i class='fas fa-shopping-cart'></i></button>
+          <button onclick="buy(${element.id})" class="btn btn-success product-cart-btn"><i class='fas fa-shopping-cart'></i></button>
       </div>
       `;
-      });
-    });
+            });
+        });
 };
 
 var cart = {};
 const buy = (id) => {
-  //not fully functional
-  cart[id] = cart[id] == null ? 1 : cart[id] + 1;
-  alert(`Added one "${items.find((x) => x.id === id).name}" to shopping cart!`);
+    //not fully functional
+    cart[id] = cart[id] == null ? 1 : cart[id] + 1;
+    alert(`Added one "${items.find((x) => x.id === id).name}" to shopping cart!`);
 };
