290e5136541832ac67ce277e388506a0f1d7119f
beebeebeeebeee <bee.1845698825@gmail.com>
2020-11-05 11:55:34 +0800

    Big fix: setTimeOut

--- web shop/main.js
+++ web shop/main.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-require('dotenv').config();
+require("dotenv").config();
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 
-
-
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 // view engine
 app.set("view engine", "ejs");
@@ -24,24 +21,23 @@ const adapter = new FileSync("./router/data/db.json");
 const db = low(adapter);
 db.defaults({ items: [], account: [], chat: [] }).write();
 
-
 // public view
 // define the home page route
 app.use(express.static(__dirname + "/public"));
 
 // use ejs view engine render /
 const index = require("./view/index");
-app.use("/", index)
-    // use ejs view engine render /product/pid/:pid
+app.use("/", index);
+// use ejs view engine render /product/pid/:pid
 const product = require("./view/product");
-app.use("/product", product)
+app.use("/product", product);
 
 // router
 // item
 const item = require("./router/item");
 app.use("/api", item);
 // account
-app.use(cookieParser('123456789'));
+app.use(cookieParser("123456789"));
 const account = require("./router/account");
 app.use("/api", account);
 
@@ -50,23 +46,23 @@ const messages = require("./router/messages");
 const { Socket } = require("dgram");
 app.use("/api", messages);
 
-
 app.get("/test", (req, res) => {
-    res.sendFile(__dirname + "/public/test.html");
+  res.sendFile(__dirname + "/public/test.html");
 });
 
-
 // app listening on port
-app.listen(port, function() {
-    console.log("Express app started on " + port);
+app.listen(port, function () {
+  console.log("Express app started on " + port);
 });
 
 //socket.io
-io.on('connection', (socket) => {
-    socket.on("sent", () => {
-        console.log("someone sent something");
-        io.emit("refresh", "refresh");
-    });
+io.on("connection", (socket) => {
+  socket.on("sent", () => {
+    console.log("someone sent something");
+    setTimeout(() => {
+      io.emit("refresh", "refresh");
+    }, 100);
+  });
 });
 
 server.listen(3001);
