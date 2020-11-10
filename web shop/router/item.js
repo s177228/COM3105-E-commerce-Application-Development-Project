4e770b5e6fc016030392357c2802b75f4e2af588
chan9948 <chan9948@gmail.com>
2020-11-10 21:24:36 +0800

    added sell-product page as test.html

--- web shop/router/item.js
+++ web shop/router/item.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
-const productsSchema = new mongoose.Schema(
-  {
+const productsSchema = new mongoose.Schema({
     pid: Number,
     sellerId: Number,
     name: String,
@@ -12,41 +11,46 @@ const productsSchema = new mongoose.Schema(
     desc: String,
     detail: String,
     buyerId: Number,
-  },
-  {
+}, {
     timestamps: true,
-  }
-);
+});
 
 const products = mongoose.model("products", productsSchema);
 
+router.get("/items/sell", function(req, res) {
+    mongoose.connect(process.env.mongoURL);
+    products.find({ sellerId: parseInt(req.signedCookies.id) })
+        .then(docs => {
+            res.send(docs);
+            mongoose.connection.close();
+        });
+});
+
 // api GET method (get all items) - /api/items/all
-router.get("/items/all", function (req, res) {
-  mongoose.connect(process.env.mongoURL);
-  products
-    .aggregate([
-      {
-        $lookup: {
-          from: "accounts",
-          localField: "sellerId",
-          foreignField: "id",
-          as: "sellerName",
-        },
-      },
-    ])
-    .then((docs) => {
-        res.send(docs);
-        mongoose.connection.close();
-    });
+router.get("/items/all", function(req, res) {
+    mongoose.connect(process.env.mongoURL);
+    products
+        .aggregate([{
+            $lookup: {
+                from: "accounts",
+                localField: "sellerId",
+                foreignField: "id",
+                as: "sellerName",
+            },
+        }, ])
+        .then((docs) => {
+            res.send(docs);
+            mongoose.connection.close();
+        });
 });
 
 // api GET method (get single item) - /api/item/123
 router.route("/item/:pid").get((req, res) => {
-  mongoose.connect(process.env.mongoURL);
-  products.find({ pid: parseInt(req.params.pid) }, (err, docs) => {
-    res.send(docs);
-    mongoose.connection.close();
-  });
+    mongoose.connect(process.env.mongoURL);
+    products.find({ pid: parseInt(req.params.pid) }, (err, docs) => {
+        res.send(docs);
+        mongoose.connection.close();
+    });
 });
 
 // api POST method (post single item) - /api/item/
@@ -55,7 +59,7 @@ router.use("/item", post_item);
 
 // api GET method (get item's image) - /api/image/123.jpg
 router.get("/image/:img", (req, res) => {
-  res.sendFile(__dirname + `/data/image/${req.params.img}`);
+    res.sendFile(__dirname + `/data/image/${req.params.img}`);
 });
 
 module.exports = router;
